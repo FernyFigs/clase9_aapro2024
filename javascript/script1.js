b22e54e4abac61dd09d1f25ca41200d0a7c29154
FernyFigs <140119637+FernyFigs@users.noreply.github.com>
2024-08-29 16:37:55 -0300

    Add files via upload

---
+++ javascript/script1.js
@@ -0,0 +1,9 @@
+// Ej 9
+
+// Función que calcule el área de un círculo
+
+let pi = Math.PI;
+let area, radio;
+radio = Number(prompt("INGRESA EL RADIO DEL CIRCULO"));
+area = pi * radio ** 2;
+alert("EL AREA DEL CIRCULO ES: " + area + " cm2");
